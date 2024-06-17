80a8218d5cb3037f01b7470849f0c43bf03b4eee
mnogodumal <raskol@duck.com>
2024-06-17 21:38:00 +0300

    17 06

---
+++ src/Components/PopUp/PopUpLanding.jsx
@@ -0,0 +1,11 @@
+export default function PopUpLanding() {
+  return (
+    <>
+      <div className="h-[100%] w-[100%] backdrop-opacity-[30px] ">
+        <div className="w-[598px] h-[679px] bg-[#1b1b1e]">
+
+        </div>
+      </div>
+    </>
+  )
+}

318b80903a24fd868bde2b62db178041c6b76964
mnogodumal <raskol@duck.com>
2024-06-22 22:42:12 +0300

    22 06

--- src/Components/PopUp/PopUpLanding.jsx
+++ src/Components/PopUp/PopUpLanding.jsx
@@ -1,7 +1,7 @@
 export default function PopUpLanding() {
   return (
     <>
-      <div style={{ background: 'rgba(20, 20, 22, 0.1)', backdropFilter: 'blur(30px)', display: 'none' }} className="h-[100vh] w-[100%] fixed z-10">
+      <div style={{ background: 'rgba(20, 20, 22, 0.1)', backdropFilter: 'blur(30px)', display: { param } }} className="h-[100vh] w-[100%] fixed z-10">
         <div className="w-[598px] h-[679px] bg-[#1b1b1e] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%]">
 
         </div>
@@ -9,3 +9,4 @@ export default function PopUpLanding() {
     </>
   )
 }
+

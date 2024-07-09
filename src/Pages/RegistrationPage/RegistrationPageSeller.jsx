12c832f0b08a867257a3171e1b32ecbd1f03cb5a
mnogodumal <raskol@duck.com>
2024-07-09 18:40:18 +0300

    09 07

---
+++ src/Pages/RegistrationPage/RegistrationPageSeller.jsx
@@ -0,0 +1,7 @@
+export default function RegistrationPageSelle() {
+  return (
+    <>
+      <h1>HELLO WORLD!!!</h1>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch } from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals';
 import LandingPage from './Pages/LandingPage/LandingPage';
+import RegistrationPageSelle from './Pages/RegistrationPage/RegistrationPageSeller';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Switch>
+
+      </Switch>
       <LandingPage />
+      <RegistrationPageSelle />
     </BrowserRouter>
   </React.StrictMode>
 );

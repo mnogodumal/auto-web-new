26cd9b294aa54b66c58f70bcf8edbc7c6ef1be72
mnogodumal <raskol@duck.com>
2024-07-29 22:46:20 +0300

    29 07

--- src/Pages/SellerPage/SellerAdsPage.jsx
+++ src/Pages/SellerPage/SellerAdsPage.jsx
@@ -3,6 +3,10 @@ import Footer from '../../Components/Footer/Footer'
 
 
 import HeaderSeller from '../../Components/Header/HeaderSeller'
+
+
+
+
 import SellerSliderProfile from './SellerSliderProfile'
 
 export default function SellerAdsPage() {
